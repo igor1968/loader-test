e38c9042f3db22d017a53e74a8564cb08319c678
IgorD <Igor.Danilchik@cib.de>
2016-09-20 09:47:30 +0300

    Leak Canary added

---
+++ app/src/main/java/com/igordanilchik/android/loader_test/LoaderTestApplication.java
@@ -0,0 +1,31 @@
+package com.igordanilchik.android.loader_test;
+
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.os.StrictMode;
+
+import com.squareup.leakcanary.LeakCanary;
+
+
+public class LoaderTestApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+
+        final boolean isDebuggable = 0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE);
+
+        if (isDebuggable) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    //.penaltyDeath()
+                    .build());
+        }
+    }
+}
